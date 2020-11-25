ceef6b2ccc2af0a2e8b9aa606b2bb24d43be7a5d
KrastevIT <barcodebit000@gmai.com>
2020-11-25 17:03:50 +0200

    Add Mobile Navbar Button Hamburger

--- src/app/components/shared/navbar/navbar.component.ts
+++ src/app/components/shared/navbar/navbar.component.ts
@@ -6,10 +6,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
-
+  isShow: boolean = false;
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  showMenu(nav: any, event: any) {
+    if (this.isShow) {
+      nav.style.display = 'none'
+      event.target.className = 'fa fa-bars'
+    } else {
+      nav.style.display = 'flex'
+      event.target.className = 'fa fa-times'
+    }
+    this.isShow = !this.isShow;
   }
 
 }
