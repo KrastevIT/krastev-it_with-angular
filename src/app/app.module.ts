c6ee7b83ed99617cbb9a605a5ef30633b641b9aa
KrastevIT <barcodebit000@gmai.com>
2020-11-25 12:40:04 +0200

    Add Animation Home Header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {RouterModule, Route} from '@angular/router';
+import {FormsModule} from '@angular/forms'
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations'
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/shared/navbar/navbar.component';
@@ -19,7 +21,9 @@ import { HomePageComponent } from './components/landing/home-page/home-page/home
     RouterModule.forRoot([
       {path: '', component: HomePageComponent},
       {path: 'home', component: HomePageComponent},
-    ])
+    ]),
+    FormsModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/landing/header/header/header.component.ts
+++ src/app/components/landing/header/header/header.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { trigger, state, animate, transition, style, keyframes } from '@angular/animations'
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
+  animations: [
+    trigger('title-animate', [
+      state('true', style({ transform: 'translateY(-109px)', opacity: 1 })),
+      transition('* <=> *', animate(3000))
+    ])
+  ]
 })
 export class HeaderComponent implements OnInit {
-
+  isOpen = true;
+ 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  onDone() {
+
+  }
 }
