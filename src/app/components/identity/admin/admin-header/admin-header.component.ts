c665b0090e96aab94313a207dc05dd2cd1cb8c81
KrastevIT <barcodebit000@gmai.com>
2020-12-11 15:56:25 +0200

    Add Admin Header Animations

--- src/app/components/identity/admin/admin-header/admin-header.component.ts
+++ src/app/components/identity/admin/admin-header/admin-header.component.ts
@@ -1,12 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { trigger, state, animate, transition, style} from '@angular/animations'
 
 @Component({
   selector: 'app-admin-header',
   templateUrl: './admin-header.component.html',
-  styleUrls: ['./admin-header.component.css']
+  styleUrls: ['./admin-header.component.css'],
+  animations: [
+    trigger('title-animate', [
+      state('true', style({ transform: ' translateY(50px)', opacity: 1 })),
+      transition('* <=> *', animate(3000))
+    ])
+  ]
 })
 export class AdminHeaderComponent implements OnInit {
-
+  isOpen = true;
+  
   constructor() { }
 
   ngOnInit(): void {
--- src/app/components/landing/home-page/header/header.component.ts
+++ src/app/components/landing/home-page/header/header.component.ts
@@ -19,8 +19,4 @@ export class HeaderComponent implements OnInit {
 
   ngOnInit(): void {
   }
-
-  onDone() {
-
-  }
 }
